882ee8bbbad17a1488758094c4812e96762635c0
Wesley Harvey <wlharvey4@mac.com>
2024-11-09 12:27:28 -0800

    Working on textproc.el; fixed a pernicious bug with a macro

--- utils/textproc/src/textproc.el
+++ utils/textproc/src/textproc.el
@@ -1,12 +1,12 @@
 ;;; textproc.el --- Process text files like cases, statutes, notes -*- mode:emacs-lisp; lexical-binding:t -*-
-;;; Time-stamp: <2024-11-08 12:04:46 lolh-mbp-16>
-
+;;; Time-stamp: <2024-11-09 12:24:38 lolh-mbp-16>
 ;;; Version: 0.0.2
+;;; Package-Requires: ((emacs "29.1") cl-lib compat)
+
 ;;; Author:   LOLH
 ;;; Created:  2024-11-07
 ;;; URL:      http://www.example.com
-;;; Keywords: text,utility
-;;; Package-Requires: ((emacs "24.1") cl-lib compat)
+;;; Keywords: tools
 
 ;;; Commentary:
 ;;  Processes files downloaded from Westlaw in .rtc format into, first,
@@ -28,6 +28,43 @@
                                      (** 1 2 "*")
                                      (+ digit)
                                      eow)))
+
+(defconst textproc-citation-re
+  (rx bow
+      (:
+       (1+ digit)
+       (1+ space)
+       (| "Wash." "Wn." "WL" "P.")
+       (* (any space "App." "2d" "3d"))
+       (: (1+ space)
+          (1+ digit))
+       eow))
+
+  "Should catch any of:
+   - 123 Wash. 456
+   - 123 Wash.2d 456
+   - 123 Wn. 456
+   - 123 Wn.2d 456
+   - 123 Wn.App. 456
+   - 123 Wn. App. 456
+   - 123 Wn.App.2d 456
+   - 123 WL 456
+   - 550 P.3d 64")
+
+(defun textproc-date-p ()
+  "Predicate function for finding a date string in a line of text."
+  (let (;; October 14, 2024 | Oct. 14, 2024
+        (ts1 (parse-time-string (buffer-substring (pos-bol) (pos-eol))))
+        ;; 10/14/2024 | 10-14-2024
+        (ts2 (when (save-excursion
+                     (re-search-forward
+                      (rx (: (= 2 (** 1 2 digit) (any "-" "/")) (= 4 digit)))
+                      (pos-eol) t))
+               t)))
+    (or (and (integerp (cl-fourth ts1))
+             (integerp (cl-fifth ts1))
+             (integerp (cl-sixth ts1)))
+        ts2)))
 
 
 (defun textproc-textutil-command (file)
@@ -70,7 +107,122 @@ Mark all page numbers as {{ **123 }}."
      (textproc-mark-case-pages-in-line)))
 
 
+(defmacro textproc-unpub-p ()
+  "Return `t' if the current case is unpublished; `nil' otherwise."
+
+  `(save-excursion
+     (goto-char (point-min))
+     (if (search-forward "note: unpublished opinion" nil t) t)))
+
+(defmacro textproc-create-list-items ()
+  "Add list item dashes to the following lines."
+
+  `(cl-loop until (eolp) do (insert "- ") (forward-line)))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun textproc-case (file &optional inter)
+  "Process a case found in FILE.
+
+INTER is `non-nil' when the command is called interactively.  In that
+case, find the file in a buffer and make it displayed."
+
+  (interactive
+   (list
+    (read-file-name "Enter a file: "
+                    textproc-process
+                    (car (directory-files textproc-downloads nil ".rtf$"))
+                    t)))
+  (let ((file (or inter (expand-file-name file textproc-process) file)))
+    (with-current-buffer (find-file-noselect file)
+      (let* ((buf (current-buffer))
+             (unpub (textproc-unpub-p))
+             (c (make-marker)))
+        (save-excursion
+          (goto-char (point-min))
+          ;; join the first and second lines to create the main heading
+          (insert "* ") (end-of-line) (insert " -- ") (delete-char 1)
+          ;; locate Document Details; create a table of contents
+          ;; TODO: link the contents to the document structure
+          (search-forward "document details")
+          (set-marker-insertion-type c t)
+          (beginning-of-line) (insert "** ") (set-marker c (pos-bol))
+          (forward-line 2)
+          (textproc-create-list-items)
+          ;; combine citations and move underneath the main headline
+          (cl-loop with m1 = (make-marker) with m2 = (make-marker)
+                   repeat 2 do
+                   (re-search-forward "citation") (set-marker m1 (pos-bol))
+                   (capitalize-word -2)
+                   (end-of-line) (insert-char 32) (delete-char 2)
+                   (set-marker m2 (+ 1 (pos-eol)))
+                   (goto-char c)
+                   (insert "- " (delete-and-extract-region m1 m2))
+                   (goto-char m1) (delete-char 1)
+                   finally (goto-char c) (insert-char 10))
+          ;; if there is a KeyCite section, create it as a section with a list
+          (when (search-forward "keycite:")
+            (beginning-of-line) (insert "** ") (forward-line 2)
+            (textproc-create-list-items)
+            (delete-region (point) (and (re-search-forward *helpers-citation-rx*) (1- (pos-bol))))
+            (beginning-of-line))
+          ;; center the caption
+          (insert "* Case" 10)
+          (cl-loop
+           ;; until (looking-at (rx bol (opt (* (any word space))) (| "Synopsis" "Opinion")))
+           until (textproc-date-p)
+           do
+           (cond
+            ((looking-at-p "|") (delete-line))
+            ((looking-at-p "No.") (open-line 1) (forward-line) (center-line) (forward-line))
+            ((looking-at-p "Only the Westlaw citation") (delete-line))
+            ((looking-at-p "\n") (forward-line))
+            (t (center-line) (forward-line)))
+           finally
+           (center-line) (forward-line) (insert-char ?\n))
+          ;;
+          (cl-loop
+           until (looking-at (rx bol (opt (* (any word blank))) (| "synopsis" "opinion")))
+           do
+           (if (looking-at-p (rx nonl))
+               (progn (if (eql (following-char) ?|)
+                          (delete-line)
+                        (progn (insert "- ") (forward-line))))
+             (forward-line))
+           finally
+           ;; add a Brief heading (brief to be completed manually)
+           (ensure-empty-lines 3)
+           (backward-char 2) (insert "** Brief") (forward-line 2)
+           ;; turn Synopsis or Opinion into a level 2 heading
+           (insert "** ")
+           (end-of-line) (unless (looking-at-p "\n\n") (insert-char ?\n)))
+          ;;
+          (unless unpub
+            (cl-loop
+             until (looking-at-p "West Headnotes")
+             do
+             (if (looking-at-p "\n")
+                 (forward-line)
+               (progn
+                 (forward-line)
+                 (unless (looking-at-p "\n")
+                   (insert-char ?\n))))
+             finally
+             (insert "** "))
+
+            ;; Turn attorney section into list,
+            ;; one item for attorneys for appellants
+            ;; and one item for attorneys for respondents
+            ;; and possibly one item for amicus curiae
+            (re-search-forward "^Attorneys and Law Firms$")
+            (beginning-of-line) (insert "** ") (forward-line) (ensure-empty-lines)
+            (debug)
+            (textproc-create-list-items)
+            (when (search-forward "amicus curiae" (pos-eol) t)
+              (beginning-of-line) (ensure-empty-lines) (insert "- ") (forward-line))
+
+            (re-search-forward "Opinion") (beginning-of-line) (insert "** ")))))))
+
 
 (defun textproc-textutil (file)
   "Convert the `.rtf' FILE into a `txt' file using `textutil' and scrub."
@@ -92,7 +244,7 @@ Mark all page numbers as {{ **123 }}."
 
 
 (defun textproc-scrub-all-lines (file &optional inter)
-  "Delete all non-breaking spaces and ensure a single space between paragraphs in FILE.
+  "Delete non-breaking spaces and ensure a single space between paragraphs in FILE.
 
 Optional INTER indicates whether the call was made interactively, in
 which case the FILE is expanded to include `textproc-process'.
@@ -119,8 +271,9 @@ The region describing the Search Details is also deleted."
                    (progn
                      (forward-line)
                      (textproc-delete-underscores-mark-pages)
-                     (when (eolp)
-                       (forward-line))))
+                     (if (eolp)
+                         (forward-line)
+                       (insert-char 10))))
                  finally
                  ;; delete the lines from `End of Document' to end of buffer
                  (delete-region (point-max) (search-backward "End of Document"))
